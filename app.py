9f60c301f2e4630112a5a25c3539d9250b2f47c2
LinaKim93 <release93@gmail.com>
2021-11-10 01:37:46 +0900

    Flask MNIST serve page (before model change)

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
+from imageio import imread
+from skimage.transform import resize
+from skimage.color import rgb2gray
 from flask import Flask, render_template, request
-import flask_restful
-import tensorflow as tf
+from PIL import Image
 import numpy as np
+import flask_restful
+import json
+import re
+import base64
+import tensorflow as tf
+
+import sys
+import os
+
+sys.path.append(os.path.abspath("./model"))
 
 app = Flask(__name__)
 api = flask_restful.Api(app)
+
+
+def parseImg(imgData):
+    # parse canvas bytes and save as output.png
+    imgstr = re.search(b"base64,(.*)", imgData).group(1)
+    with open("output.jpg", "wb") as output:
+        output.write(base64.decodebytes(imgstr))
+
+
+def testImg():
+    # img = imread("output.jpg") / 255
+    img = imread("test.jpg") / 255
+    print(img.shape)
+    img = rgb2gray(img)
+    img = resize(img, (1, 28, 28, 1))
+    model = tf.keras.models.load_model("./model/mnist.h5")
+
+    pred = model.predict(img)
+    print(pred.argmax())
 
 
 @app.route("/")
@@ -13,13 +44,29 @@ def index():
     return render_template("index.html", title=title)
 
 
-@app.route("/letter", methods=["POST"])
-def letter():
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
     if request.method == "POST":
-        letter = request.form
-        return letter
+        data = request.get_data()
+        parseImg(data)
+        return "POST"
+
+    if request.method == "GET":
+        img = imread("output.jpg") / 255
+        img = rgb2gray(img)
+        img = resize(img, (1, 28, 28, 1))
+
+        model = tf.keras.models.load_model("./model/mnist.h5")
+
+        pred = model.predict(img)
+        print(pred.argmax())
+
+        result = pred.argmax()
+
+        return str(result)
 
 
 if __name__ == "__main__":
     print("server run")
-    app.run(host="localhost", port="5000", debug=True)
+    testImg()
+    # app.run(host="localhost", port="5000", debug=True)
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,10 +1,13 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
+ctx.clearRect(0, 0, 500, 500);
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 const draw = (e) => {
   const x = e.offsetX;
   const y = e.offsetY;
   if (!drawing) return;
-  ctx.lineWidth = 20;
+  ctx.lineWidth = 30;
   ctx.lineCap = "round";
   ctx.strokeStyle = "#000";
   ctx.beginPath();
@@ -21,3 +24,54 @@ canvas.addEventListener("mouseup", () => {
   drawing = false;
 });
 canvas.addEventListener("mousemove", draw);
+
+const predict_btn = document.querySelector(".result");
+const clear_btn = document.querySelector(".clear");
+const result = document.querySelector("#result");
+
+result.textContent = "result : ";
+
+predict_btn.addEventListener("click", function () {
+  result.textContent = "Predicting...";
+  let canvasObj = document.getElementById("canvas");
+  let img = canvasObj.toDataURL("image/url");
+  let data = {
+    image: img,
+  };
+  sendImg(data);
+  getImg();
+});
+
+clear_btn.addEventListener("click", function () {
+  ctx.clearRect(0, 0, 500, 500);
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+
+function sendImg(data) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "./predict", true);
+  xhr.onload = function () {
+    if (xhr.status === 200 || xhr.status === 201) {
+      console.log(xhr.responseText);
+    } else {
+      console.error(xhr.responseText);
+    }
+  };
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(JSON.stringify(data));
+}
+
+function getImg() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "./predict", true);
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      console.log(xhr.response);
+      result.textContent = `result : ${xhr.response}`;
+    } else {
+      console.log("fail");
+    }
+  };
+  xhr.send();
+}
